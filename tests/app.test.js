4a2989d465ca16049d8656b4f534585acd586092
eebbesen <eebbesen.git@gmail.com>
2018-11-02 13:45:25 -0500

    randomize selection

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,5 +2,5 @@ const app = require('../app');
 
 test('runs query', async () => {
   const text = await app.getText();
-  expect(text).toContain('Our Statement of Rights and Responsibilities is changing to our Terms of Service');
+  expect(text).toContain('the');
 });
