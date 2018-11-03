0c91387ffac005314f103a6f1dec114e2aff3135
J2EEbbesen <eebbesen@users.noreply.github.com>
2018-11-03 13:01:36 -0500

    testing, more data, and randomized return

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 'use strict'
 
+const AWS = require('aws-sdk');
 const env = process.env.NODE_ENV || 'development';
+const fsPromises = require('fs').promises;
+const Polly = new AWS.Polly({
+  signatureVersion: 'v4',
+  region: 'us-east-1'
+});
 
 // database
 const dbConfig = require('./db/config');
 const { Client } = require('pg');
 const client = new Client(dbConfig[env]);
 
+// returns random EULA text
 const getText = async function() {
   let rows;
   try {
     await client.connect();
-    rows = await client.query('SELECT content FROM eulas');
+    rows = await client.query('SELECT content FROM eulas OFFSET floor(random()*(select count(*) from eulas)) LIMIT 1');
     await client.end();
   } catch (err) {
     console.log('ERROR in getText', err);
@@ -19,4 +26,34 @@ const getText = async function() {
   return rows.rows[0]['content'];
 }
 
+// returns a promise to create an mp3 from text
+const synthesizeSpeech = function(text) {
+  const params = {
+    'Text': text,
+    'OutputFormat': 'mp3',
+    'VoiceId': 'Kimberly'
+  };
+  return Polly.synthesizeSpeech(params).promise();
+}
+
+const createMp3 = function(text, name) {
+  return synthesizeSpeech(text)
+    .then((data, err) => {
+      if (err) {
+        console.log('Error in speech synthesis of createMp3', err);
+      } else if (data) {
+        if (data.AudioStream instanceof Buffer) {
+          fsPromises.writeFile(`${name}.kimberly.mp3`, data.AudioStream)
+        }
+      } else {
+        throw 'createMp3: data not a Buffer!!';
+      }
+    })
+    .catch(err => {
+      console.log('error in createMp3', err);
+    });
+}
+
 module.exports.getText = getText;
+module.exports.createMp3 = createMp3;
+module.exports.synthesizeSpeech = synthesizeSpeech;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,37 @@
 const app = require('../app');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+const filename = 'hello_test.kimberly.mp3';
+
+function cleanup() {
+  try {
+    if (fs.statSync(filename)) {
+      fs.unlinkSync(filename);
+    }
+  } catch (err) {
+    // nothing
+  }
+}
+
+beforeEach(() => {
+  cleanup();
+});
+
+afterEach(() => {
+  cleanup();
+});
 
 test('runs query', async () => {
   const text = await app.getText();
-  expect(text).toContain('Our Statement of Rights and Responsibilities is changing to our Terms of Service');
+  expect(text).toContain('the');
+});
+
+test('creates mp3', () => {
+  const ret = app.createMp3('This is some text', 'hello_test');
+
+  return ret.then((data, err) => {
+    return expect(fsPromises.stat(filename)).resolves.toBeDefined();
+  });
 });
