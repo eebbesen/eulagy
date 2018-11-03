c177a96d8df17b0bc8cb0c13ff4088e1e94d1e08
eebbesen <eebbesen.git@gmail.com>
2018-11-03 12:27:16 -0500

    working, but test does not fail on failure

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const AWS = require('aws-sdk');
 const env = process.env.NODE_ENV || 'development';
+const fs = require('fs');
+const fsPromises = fs.promises;
+const Polly = new AWS.Polly({
+  signatureVersion: 'v4',
+  region: 'us-east-1'
+});
 
 // database
 const dbConfig = require('./db/config');
 const { Client } = require('pg');
 const client = new Client(dbConfig[env]);
 
+// returns random EULA text
 const getText = async function() {
   let rows;
   try {
@@ -19,4 +27,34 @@ const getText = async function() {
   return rows.rows[0]['content'];
 }
 
+// returns a promise to create an mp3 from text
+const synthesizeSpeech = function(text) {
+  const params = {
+    'Text': text,
+    'OutputFormat': 'mp3',
+    'VoiceId': 'Kimberly'
+  };
+  return Polly.synthesizeSpeech(params).promise();
+}
+
+const createMp3 = function(text, name) {
+  return synthesizeSpeech(text)
+    .then((data, err) => {
+      if (err) {
+        console.log('Error in speech synthesis of createMp3', err);
+      } else if (data) {
+        if (data.AudioStream instanceof Buffer) {
+          fsPromises.writeFile(`${name}.kimberly.mp3`, data.AudioStream)
+        }
+      } else {
+        throw 'createMp3: data not a Buffer!!';
+      }
+    })
+    .catch(err => {
+      console.log('error in createMp3', err);
+    });
+}
+
 module.exports.getText = getText;
+module.exports.createMp3 = createMp3;
+module.exports.synthesizeSpeech = synthesizeSpeech;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,27 @@
 const app = require('../app');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
 test('runs query', async () => {
   const text = await app.getText();
   expect(text).toContain('the');
+});
+
+test('creates mp3', () => {
+  const filename = '/Users/eebbesen/p3/eulagy/hello_test.kimberly.mp3';
+  if (fs.existsSync(filename)){
+    fs.unlinkSync(filename);
+  }
+
+  const ret = app.createMp3('hello there', 'hello_test');
+
+  return ret.then((data, err) => {
+    fsPromises.stat(filename)
+      .then(ret => {
+        // just checking to make sure value is there
+        expect(ret.mode).toEqual(33188);
+        fsPromises.unlink(filename)
+      })
+  })
 });
