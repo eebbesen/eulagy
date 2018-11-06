b0205f1c92039d5744206dc3311d48ca9ec4f1bc
eebbesen <eebbesen.git@gmail.com>
2018-11-05 20:07:50 -0600

    more testing

--- app.js
+++ app.js
@@ -2,49 +2,29 @@
 
 const AWS = require('aws-sdk');
 const env = process.env.NODE_ENV || 'development';
-// const fsPromises = require('fs').promises;
 const Polly = new AWS.Polly({
   signatureVersion: 'v4',
   region: 'us-east-1'
 });
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const s3Helper = require('./lib/bucket_utils');
 
-function downloadFile(name) {
-  const params = { Bucket: 'eulagy', Key: name };
-  return s3.getObject(params, function (err, data) {
-    if (err) {
-      console.log("Error", err, data);
-    }
-  }).promise();
-}
-
-// uploads one file to S3 bucket
-const uploadFile = function(name, data) {
-  const params = { Bucket: 'eulagy', Key: name, Body: data };
-  return s3.upload (params, function (err, data) {
-    if (err) {
-      console.log("Error", err, data);
-    }
-  }).promise();
-}
 
 const handler = function(event) {
   const rec = event.Records[0];
   const fileName = rec.s3.object.key;
-  let text
+  let text;
 
-  return downloadFile(fileName)
-  .then(data => {
-    text = data.Body.toString()
-  })
-  .then(() => {
-    const rr = synthesizeSpeech(text)
-    return rr
-  })
-  .then(mp3 => {
-    // upload to S3
-    return uploadFile(`uploaded/${fileName.replace('txt', 'mp3')}`, mp3.AudioStream);
-  });
+  return s3Helper.downloadFile(fileName)
+    .then(data => {
+      return data.Body.toString();
+    })
+    .then((text) => {
+      return synthesizeSpeech(text);
+    })
+    .then(mp3 => {
+      return s3Helper
+        .uploadFile(`uploaded/${fileName.replace('txt', 'mp3')}`, mp3.AudioStream);
+    });
 };
 
 // returns a promise to create an mp3 from text
@@ -57,24 +37,5 @@ const synthesizeSpeech = function(text) {
   return Polly.synthesizeSpeech(params).promise();
 };
 
-const createMp3 = function(text, name) {
-  return synthesizeSpeech(text)
-    .then((data, err) => {
-      if (err) {
-        console.log('Error in speech synthesis of createMp3', err);
-      } else if (data) {
-        if (data.AudioStream instanceof Buffer) {
-          fsPromises.writeFile(`${name}.kimberly.mp3`, data.AudioStream);
-        }
-      } else {
-        throw 'createMp3: data not a Buffer!!';
-      }
-    })
-    .catch(err => {
-      console.log('error in createMp3', err);
-    });
-};
-
-module.exports.createMp3 = createMp3;
 module.exports.synthesizeSpeech = synthesizeSpeech;
 module.exports.handler = handler;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,43 +1,28 @@
 const app = require('../app');
 const AWS = require('aws-sdk');
-const fs = require('fs');
 const fsPromises = require('fs').promises;
+const s3Helper = require('../lib/bucket_utils');
 
-const filename = 'hello_test.kimberly.mp3';
-
-function cleanup() {
-  try {
-    if (fs.statSync(filename)) {
-      fs.unlinkSync(filename);
-    }
-  } catch (err) {
-    // nothing
-  }
-}
-
-beforeEach(() => {
-  cleanup();
-});
-
-afterEach(() => {
-  cleanup();
-});
-
-// live test, requires file humegatech-11.05.2018.txt in eulagy root in S3
-test('handler does it all', () => {
+// live test, requires file humegatech-11.05.2018.txt in eulagy root in S3 so we upload it
+test('handler creates mp3', () => {
   const event = {
     Records: [{
       s3: {
         object: {
-          key: 'humegatech-11.05.2018.txt'
+          key: 'humegatech-11.05.2018.txt.slug'
         }
       }
     }]
   };
 
-  // console.log('ddddddoooooonnnneee', app.handler(event))
-  app.handler(event)
-    .then(e => {
-      console.log('ddddddoooooonnnneee', e);
+  return fsPromises.readFile('tests/files/humegatech-11.05.2018.txt.slug')
+    .then((buffer, err) => {
+      s3Helper.uploadFile('humegatech-11.05.2018.txt.slug', buffer)
     })
+    .then(() => {
+      return app.handler(event)
+    })
+    .then((file) => {
+      expect(file.key).toEqual('uploaded/humegatech-11.05.2018.mp3.slug');
+    });
 });
