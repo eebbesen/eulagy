ad758f7444400a87342e3df91ba3f3661020de59
J2EEbbesen <eebbesen@users.noreply.github.com>
2018-11-06 22:31:51 -0600

    AWS Comprehend (#17)
    * modularizing file utils
    
    * Adding Comprehend
    
    * Adding sentiment
    
    * sorting key phrases
    
    * creating csv from comprehend results

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const AWS = require('aws-sdk');
+const Comprehend = new AWS.Comprehend({
+  apiVersion: '2017-11-27',
+  region: 'us-east-1'
+});
 const Polly = new AWS.Polly({
   signatureVersion: 'v4',
   region: 'us-east-1'
@@ -11,28 +15,41 @@ const s3Helper = require('./lib/bucket_utils');
 const handler = function(event) {
   const rec = event.Records[0];
   const fileName = rec.s3.object.key;
+
+  let text;
   let createDetails;
 
   return s3Helper.downloadFile(fileName)
     .then(data => {
       return data.Body.toString();
     })
-    .then((text) => {
-      const chunks = text.match(/[\s\S]{1,2999}/g)
+    .then((eulaText) => {
+      text = eulaText;
+      const chunks = eulaText.match(/[\s\S]{1,2999}/g)
       return synthesizeSpeech(chunks[0]);
     })
     .then(mp3 => {
       return s3Helper
         .uploadFile(`uploaded/${fileName.replace('txt', 'mp3')}`, mp3.AudioStream);
     })
-    .then((cd) => {
+    .then(cd => {
       createDetails = cd;
       try {
         s3Helper
           .deleteFile(fileName);
       } catch (err) {
-        console.log(`Issue deleting ${fileName}`);
+        console.log(`EULAGY:: Issue deleting ${fileName}`);
       }
+    })
+    .then(() => {
+      const chunks = text.match(/[\s\S]{1,4900}/g);
+      return detectKeyPhrases(chunks[0]);
+    })
+    .then(kp => {
+      const vals = sortEntriesByValues(kp.KeyPhrases);
+      const csv = mapToCsv(vals);
+      return s3Helper
+        .uploadFile(`uploaded/${fileName.replace('txt', 'csv')}`, csv);
     })
     .then(() => {
       return createDetails
@@ -49,5 +66,39 @@ const synthesizeSpeech = function(text) {
   return Polly.synthesizeSpeech(params).promise();
 };
 
-module.exports.synthesizeSpeech = synthesizeSpeech;
+const detectSentiment = function(text) {
+  const params = {
+    LanguageCode: 'en',
+    Text: text
+  };
+  return Comprehend.detectSentiment(params).promise();
+};
+
+const detectKeyPhrases = function(text) {
+  const params = {
+    LanguageCode: 'en',
+    Text: text
+  };
+  return Comprehend.detectKeyPhrases(params).promise();
+};
+
+// count and sort map by count of key (lower case)
+const sortEntriesByValues = function(arr) {
+  const occ = arr.reduce((occ, val) => occ.set(val.Text.toLowerCase(), 1 + (occ.get(val.Text.toLowerCase()) || 0)), new Map());
+  return new Map([...occ.entries()].sort((a, b) => b[1] - a[1]));
+};
+
+const mapToCsv = function(map) {
+  let text = '';
+  map.forEach((v,k,m) => {
+    text += `${k},${v}\n`;
+  });
+  return text;
+};
+
 module.exports.handler = handler;
+module.exports.detectKeyPhrases = detectKeyPhrases;
+module.exports.detectSentiment = detectSentiment;
+module.exports.mapToCsv = mapToCsv;
+module.exports.sortEntriesByValues = sortEntriesByValues;
+module.exports.synthesizeSpeech = synthesizeSpeech;
--- tests/app.test.js
+++ tests/app.test.js
@@ -26,3 +26,103 @@ test('handler creates mp3', () => {
       expect(file.key).toEqual('uploaded/humegatech-11.05.2018.mp3.slug');
     });
 });
+
+test('detects sentiment of facebook', () => {
+  return fsPromises.readFile('eulas/facebook/04.19.2018.txt')
+    .then((buffer, err) => {
+      const text = buffer.toString();
+      const chunks = text.match(/[\s\S]{1,4999}/g)
+      return app.detectSentiment(chunks[0]);
+    })
+    .then((sentiment) => {
+      // console.log('FACEBOOK', sentiment);
+      expect(sentiment.Sentiment).toEqual('NEUTRAL');
+    });
+});
+
+test('detects sentiment of tumblr', () => {
+  return fsPromises.readFile('eulas/tumblr/05.15.2018.txt')
+    .then((buffer, err) => {
+      const text = buffer.toString();
+      const chunks = text.match(/[\s\S]{1,4899}/g)
+      return app.detectSentiment(chunks[0]);
+    })
+    .then((sentiment) => {
+      // console.log('TUMBLR', sentiment);
+      expect(sentiment.Sentiment).toEqual('NEUTRAL');
+    });
+});
+
+test('detects key phrases in facebook eula', () => {
+  return fsPromises.readFile('eulas/facebook/04.19.2018.txt')
+    .then((buffer, err) => {
+      const text = buffer.toString();
+      const chunks = text.match(/[\s\S]{1,4999}/g)
+      return app.detectKeyPhrases(chunks[0]);
+    })
+    .then((keyPhrases) => {
+      expect(keyPhrases.KeyPhrases.length).toBeGreaterThan(10);
+    });
+});
+
+test('sorts key phrases by count descending', () => {
+  const kp = [ { Score: 0.9989810585975647,
+        Text: 'Our Statement',
+        BeginOffset: 0,
+        EndOffset: 13 },
+      { Score: 0.9646416306495667,
+        Text: 'Rights and Responsibilities',
+        BeginOffset: 17,
+        EndOffset: 44 },
+      { Score: 0.9958744049072266,
+        Text: 'our STATEMENT',
+        BeginOffset: 60,
+        EndOffset: 69 },
+      { Score: 0.9955416321754456,
+        Text: 'Service',
+        BeginOffset: 73,
+        EndOffset: 80 },
+      { Score: 0.9994470477104187,
+        Text: 'our previous Statement',
+        BeginOffset: 95,
+        EndOffset: 117 } ];
+
+  const sorted = app.sortEntriesByValues(kp);
+
+  expect(sorted.size).toBe(4);
+  expect(sorted.get('our statement')).toBe(2);
+  expect(sorted.get('rights and responsibilities')).toBe(1);
+  expect(sorted.get('service')).toBe(1);
+  expect(sorted.get('our previous statement')).toBe(1);
+});
+
+test('Map to csv', () => {
+  const kp = [ { Score: 0.9989810585975647,
+        Text: 'Our Statement',
+        BeginOffset: 0,
+        EndOffset: 13 },
+      { Score: 0.9646416306495667,
+        Text: 'Rights and Responsibilities',
+        BeginOffset: 17,
+        EndOffset: 44 },
+      { Score: 0.9958744049072266,
+        Text: 'our STATEMENT',
+        BeginOffset: 60,
+        EndOffset: 69 },
+      { Score: 0.9955416321754456,
+        Text: 'Service',
+        BeginOffset: 73,
+        EndOffset: 80 },
+      { Score: 0.9994470477104187,
+        Text: 'our previous Statement',
+        BeginOffset: 95,
+        EndOffset: 117 } ];
+
+  const map = app.sortEntriesByValues(kp);
+
+  const result = app.mapToCsv(map);
+
+  expect(result.replace(/\n/g, '')).toEqual('our statement,2rights and responsibilities,1service,1our previous statement,1');
+});
+
+// find word(s) that appear very seldom to flag unusual clauses...
