d10dbae74d83b7e6ff1d33f904a3b7bc4f417f5b
eebbesen <eebbesen.git@gmail.com>
2023-11-28 13:29:29 -0600

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "ignorePatterns": [".eslintrc.js"],
+    "rules": {
+        "semi": "off",
+        "@typescript-eslint/semi": "warn"
+    }
+}
