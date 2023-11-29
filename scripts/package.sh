cae9e2d91dc10b1839127c3019c977904d787f54
eebbesen <eebbesen.git@gmail.com>
2023-11-29 16:53:06 -0600

    Updating packaging mechanism

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+set -e
+setopt localoptions rmstarsilent
+
 echo 'removing node_modules'
-rm -rf node_modules/* > /dev/null
+rm -rf node_modules/* 
 echo 'done clearing node_modules'
 
 echo 'npm install --omit=dev'
-npm install --omit=dev > /dev/null
-echo 'npm install complete'
+npm install --omit=dev       
 
 echo 'running tsc to generate .js files for prod only'
 rm -rf distProd/*
 tsc -p tsconfig.prod.json
-echo 'production .js in ./distProd'
+echo 'production .js in ./distProd/src'
 
-export VER=`grep version package.json | perl -nle 'print $& if m{\\d.\\d.\\d}'`
+VER=`grep version package.json | perl -nle 'print $& if m{\\d.\\d.\\d}'`
 echo "release version will be ${VER}"
 
-zip -r builds/eulagy-$VER.zip package.json node_modules > /dev/null
-zip -j builds/eulagy-$VER.zip distProd/*.js > /dev/null
+PTH=`pwd`
+zip -r builds/eulagy-$VER.zip package.json node_modules 2>&1 1>/dev/null
+cd distProd/src
+zip builds/eulagy-$VER.zip ./**/*.js 2>&1 1>/dev/null
 echo "release package builds/eulagy-${VER}.zip"
 
 echo 'npm install again to reset development'
 npm install
+
+echo 'build complete!'
