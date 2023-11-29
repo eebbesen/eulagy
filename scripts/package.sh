a3bf4b7de363661e9eb470dcf5dba5c7a765cb24
eebbesen <eebbesen.git@gmail.com>
2023-11-29 11:14:03 -0600

    savepoint

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 echo 'removing node_modules'
-rm -rf node_modules/*
-npm install --omit=dev
+rm -rf node_modules/* > /dev/null
+echo 'done clearing node_modules'
+
+echo 'npm install --omit=dev'
+npm install --omit=dev > /dev/null
+echo 'npm install complete'
+
 echo 'running tsc to generate .js files for prod only'
 rm -rf distProd/*
 tsc -p tsconfig.prod.json
+echo 'production .js in ./distProd'
+
 export VER=`grep version package.json | perl -nle 'print $& if m{\\d.\\d.\\d}'`
-zip -r builds/eulagy-$VER.zip package.json node_modules
-zip -j builds/eulagy-$VER.zip distProd/*.js
+echo "release version will be ${VER}"
+
+zip -r builds/eulagy-$VER.zip package.json node_modules > /dev/null
+zip -j builds/eulagy-$VER.zip distProd/*.js > /dev/null
+echo "release package builds/eulagy-${VER}.zip"
+
+echo 'npm install again to reset development'
 npm install
